3bd715df53417c2851e85f8060e5fb654b3c768b
Stackbit Code Editor <projects@stackbit.com>
2021-03-19 19:54:43 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         }
 
         if (window.onNextjsRouteChangeComplete) {
-            window.onNextjsRouteChangeComplete();
+            window.onNextjsRouteChangeComplete(mirror);
         }
 
         const handleRouteChangeStart = () => {
