299a378d0ba16e494ac6b03bc00900f00646b3f1
Stackbit Code Editor <projects@stackbit.com>
2021-03-19 19:54:02 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const sourcebit = require('sourcebit');
 
-const sourcebitConfig = require('./sourcebit.js');
+constre sourcebitConfig = require('./sourcebit.js');
 
 
 sourcebit.fetch(sourcebitConfig);
